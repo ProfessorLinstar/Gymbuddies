07ff7928c66a96d378c657ca3042eb8b7344f250
Jason Oh <jasonohin1113@gmail.com>
2022-10-14 10:26:02 -0400

    signatures

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,20 +1,46 @@
 """Database API"""
 
 #### User creation ####
-# create_user(user info...)
-# update_user(userid, user info...)
-# delete_user(userid)
+# create user using relevant information
+def create_user(userid, userinfo...) -> ok
+# update user info (listed in the Users table)
+def update_user(userid, userinfo...) -> ok
+# remove user from database
+def delete_user(userid) -> ok
+
+### User information ###
+# return name associated with userid
+def get_name(userId: int) -> str
+# return bio associated with userid
+def get_bio(userId: int) -> str
+# return level assciate with userid
+def get_level(userId: int) -> str
+# return additional into associated with userid
+def get_additionalInfo(userId: int) -> str
 
 #### Matching ####
-# query_matches(user)
-# incoming_requests(destuser)
-# outgoing_requests(srcuser)
-# make_request(srcuser, destuser, times)
-# accept_request(requestid)
-# delete_request(srcuser, destuser, times)
+# get a list of matches associated with a user
+def get_matches(srcUserId: int) -> dict[destUserId: int, status: int]
+# get a list of incoming matches associated with a user
+def incoming_requets(srcUserId: int) -> dict[destUserId: int, status: int]
+# get a list of outgoing matches associated with a user
+def outgoing_requets(srcUserId: int) -> dict[destUserId: int, status: int]
+# make an outgoing request to another user with specific times
+def make_request(srcUserId: int, destUserId: int, times: list[Time])
+# accept incoming request
+def accept_request(srcUserId: int, requestId: int)
+# delete outgoing request
+def delete_request(srcUserId: int, requestId: int)
 
 #### Scheduling ####
-# get_schedule(user)
-# add_time(user)
-# delete_time(user)
-# parse_schedule(schedule: str)
+# return schedule informing occupied times of user
+# list will be of size 2016 (7 * 24 * 12)
+def get_schedule(userId: int) -> list[str]
+# update a time to be occupied on userend
+def add_time(userId: int, time: Time, eventtype: str) -> ok
+# update a time to be available on userend
+def delete_time(user) -> ok
+# return schedule from str to an object form
+def parse_schedule(schedule: str) -> list[str]
+# return schedule specifically showing time of matches
+def get_match_schedule(userId: int) -> list[str]
