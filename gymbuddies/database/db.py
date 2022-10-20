d69ded56316180951ac56e42a9d9098ea7ed3f85
ProfessorLinstar <awaw6465@gmail.com>
2022-10-20 17:26:36 -0400

    database status enums

--- gymbuddies/database/db.py
+++ gymbuddies/database/db.py
@@ -1,4 +1,5 @@
 """Tests database initialization."""
+from enum import Enum
 from sqlalchemy import Column, String, Integer, Boolean, PickleType
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,7 +9,7 @@ NUM_TIME_BLOCKS = 2016
 
 
 class User(BASE):
-    """Users database"""
+    """Users database. Maps each userid to their Gymbuddies profile information."""
     __tablename__ = "users"
 
     userid = Column(Integer, primary_key=True)  # unique user id
@@ -21,11 +22,11 @@ class User(BASE):
     schedule = Column(String)  # a sequence of 2016 characters indicating status for each time block
     open = Column(Boolean)  # open for matching
 
-
     settings = Column(PickleType)  # Notification and account settings
 
+
 class Request(BASE):
-    """Requests table"""
+    """Requests table. Includes requests that are pending, rejected, or finalized (i.e. completed matches)."""
     __tablename__ = "requests"
 
     requestid = Column(Integer, primary_key=True)  # unique request transaction id
@@ -37,9 +38,40 @@ class Request(BASE):
 
 
 class Schedule(BASE):
-    """Schedule table"""
+    """Schedule table. Maps each 5-minute time block throughout the week to users and statuses."""
     __tablename__ = "schedule"
 
     timeblock = Column(Integer, primary_key=True)  # a particular time block (5-minute granularity => 2016 blocks)
     userid = Column(Integer, primary_key=True)  # userid for a particular time block
     status = Column(String)  # status of userid for this time block (e.g. 0 = available, 1 = already matched)
+
+
+class RequestStatus(Enum, int):
+    """Request status enumeration. Includes pending, rejected, and finalized requests. Requests that become matches have a value of at least
+    RequestStatus.FINALIZED. When reading the a request status from the Requests database, it should be cast to a RequestStatus enumeration.
+    RequestStatus can have 5 states:
+        0 - REJECTED:   request was rejected before finalization
+        1 - PENDING:    request is currently pending
+        2 - RETURN:     request was modified by destination user, and is being returned for confirmation
+        3 - FINALIZED:  request has been finalized (match completed)
+        4 - TERMINATED: request was finalized, but has been terminated.
+    """
+
+    REJECTED = 0
+    PENDING = 1
+    RETURN = 2
+    FINALIZED = 3
+    TERMINATED = 4
+
+class ScheduleStatus(Enum, str):
+    """Time block status enumeration. Indicates user status in a particular time block. ScheduleStatus can have 4 states:
+        0 - UNAVAILABLE: user indicated that they are not available at this time
+        1 - AVAILABLE:   user indicated that they are available at this time, and they have no pending or finalized matches here
+        2 - PENDING:     user indicated that they are available at this time, but they are waiting on a request for this time
+        3 - MATCHED:     user indicated that they are available at this time, but they have already been matched for this time
+    """
+
+    UNAVAILABLE = 0
+    AVAILABLE = 1
+    PENDING = 2
+    MATCHED = 3
