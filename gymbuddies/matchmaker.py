e188fcf5500288ee055ea4c042e1706fa231f3dd
Jason Oh <jasonohin1113@gmail.com>
2022-10-28 16:53:35 -0400

    commit for matchmaker.py

--- gymbuddies/matchmaker.py
+++ gymbuddies/matchmaker.py
@@ -9,6 +9,7 @@ import database.db
 
 # TODO: IMPORTANT! NUMBER_INTERESTS SHOULD NOT BE HARDCODED. CHANGE THIS ASAP!!!
 NUMBER_INTERESTS: int = 10 # TODO: find a way for this value not to be hardcoded
+NUMBER_SCHEDULE_BLOCKS: int = 2016 # number of timeblocks contained in a user's schedule
 RANDOM_NUMBER: int = 10 # number of users queried in random selection
 RETURN_NUMBER: int = 3 # number of users returned by find_matches
 LEVEL_WEIGHT: float = 0.5 # weight of level to compatability score
@@ -26,6 +27,8 @@ def find_matches(netid: str) -> List[str]:
         for user in randusers:
             matches.append(user.netid)
         return matches
+
+
     # record user compatability scores based on user levels and level preferences
     user_compatabilities: Dict[str, float] = {}
     main_user_level = database.user.get_level(netid)
@@ -34,6 +37,7 @@ def find_matches(netid: str) -> List[str]:
     for user in randusers:
         user_level = database.user.get_level(user.netid)
         assert user_level is not None
+
         # record whether user is compatable with the level preference of the main user
         if main_user_level_preference is database.db.LevelPreference.EQUAL:
             if main_user_level == user_level:
@@ -44,6 +48,7 @@ def find_matches(netid: str) -> List[str]:
         elif main_user_level_preference is database.db.LevelPreference.GREATEREQUAL:
             if main_user_level <= user_level:
                 user_compatabilities[user.netid] += 1
+
         # record whether the main user is compatable with the level preference of user
         user_level_preference = database.user.get_level_preference(netid)
         if user_level_preference is database.db.LevelPreference.EQUAL:
@@ -55,8 +60,11 @@ def find_matches(netid: str) -> List[str]:
         elif user_level_preference is database.db.LevelPreference.GREATEREQUAL:
             if user_level <= main_user_level:
                 user_compatabilities[user.netid] += 1
+
         # adjust user compatability score with weight
         user_compatabilities[user.netid] *= LEVEL_WEIGHT
+
+
     # update user compatability scores based on user interests
     main_user_interests = database.user.get_interests(netid)
     assert main_user_interests is not None
@@ -68,12 +76,21 @@ def find_matches(netid: str) -> List[str]:
             if user_interests[interest] == main_user_interests[interest]:
                 interests_score += 1
         user_compatabilities[user.netid] += interests_score * INTERESTS_WEIGHT
+
+
     # update user compatability scores based on schedule intersection
     main_user_schedule: List[int] | None = database.schedule.get_schedule(netid)
     assert main_user_schedule is not None
+    schedule_score: float = 0
     for user in randusers:
         user_schedule: List[int] | None = database.schedule.get_schedule(user.netid)
         assert user_schedule is not None
+        for i in range(len(user_schedule)):
+            if main_user_schedule[i] & database.db.ScheduleStatus.AVAILABLE == 1\
+            and user_schedule[i] & database.db.ScheduleStatus.AVAILABLE == 1:
+                schedule_score += 1
+            user_compatabilities[user.netid] += (
+                schedule_score / NUMBER_SCHEDULE_BLOCKS) * SCHEDULE_WEIGHT
 
 
     # return users with the highest compatabilties to the main user
