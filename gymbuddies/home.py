d2bb5bdf6c2b597be3e6ce813c1a9700b79af17f
ProfessorLinstar <awaw6465@gmail.com>
2022-11-04 02:22:33 -0400

    added common.py

--- gymbuddies/home.py
+++ gymbuddies/home.py
@@ -4,8 +4,9 @@ from flask import Blueprint
 from flask import session
 from flask import request
 from flask import render_template, redirect, url_for
-from .database import db
+from . import common
 from . import database
+from .database import db
 
 bp = Blueprint("home", __name__, url_prefix="")
 
