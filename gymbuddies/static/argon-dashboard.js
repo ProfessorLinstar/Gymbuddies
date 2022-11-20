8c56bcd6aa4301ef45df892ad695a6d510ee586e
ProfessorLinstar <awaw6465@gmail.com>
2022-11-20 11:01:10 -0500

    argon hamburger fix

--- gymbuddies/static/argon-dashboard.js
+++ gymbuddies/static/argon-dashboard.js
@@ -400,7 +400,7 @@ function toggleSidenav() {
 let html = document.getElementsByTagName('html')[0];
 
 html.addEventListener("click", function(e) {
-  if (body.classList.contains('g-sidenav-pinned') && !e.target.classList.contains('sidenav-toggler-line')) {
+  if (body.classList.contains('g-sidenav-pinned') && !e.target.classList.contains('sidenav-toggler-inner')) {
     body.classList.remove(className);
   }
 });
