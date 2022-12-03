7f6c4d316f328a8f405dda6c3fd5e6b91079c370
Jason Oh <jasonohin1113@gmail.com>
2022-12-03 04:58:53 -0500

    push

--- gymbuddies/sendsms.py
+++ gymbuddies/sendsms.py
@@ -14,16 +14,17 @@ def sendsms(number: str, message: str) -> bool:
     numberstr = "+" + number
     if len(numberstr) != 12:
         return False
-    print(numberstr[1])
+    print("got here 1")
     if numberstr[1] != "1":
         return False
+    print("got here 2")
     try:
         account_sid = os.environ['TWILIO_ACCOUNT_SID']
         auth_token = os.environ['TWILIO_AUTH_TOKEN']
         from_number = os.environ['TWILIO_SMS_NUMBER']
         client = Client(account_sid, auth_token)
         message = client.messages.create(body=message, from_=from_number, to=number)
-        print("got here")
+        print("got here 3")
         return True
     except Exception as ex:
         print(ex)
