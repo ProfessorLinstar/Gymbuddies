3f600eb5b94bc7cb8ed35f0df4e963d78310f451
ProfessorLinstar <awaw6465@gmail.com>
2022-12-13 16:23:09 -0500

    Revert "csrf protection"
    This reverts commit 70f315f046170521ca0048ec7a6230a30d7b623a.

--- gymbuddies/__init__.py
+++ gymbuddies/__init__.py
@@ -1,7 +1,6 @@
 """Gymbuddies Flask web application."""
 import os
 from flask import Flask
-from flask_wtf import csrf
 from . import home, master, auth, matching, error
 from . import database
 from .database import db
@@ -13,7 +12,6 @@ def create_app():
     app = Flask(__name__)
     extension.mail.init_app(app)
     app.config.from_mapping(SECRET_KEY=os.getenv("FLASK_SECRET_KEY", "dev"))
-    csrf.CSRFProtect(app)
 
     app.register_blueprint(home.bp)
     app.register_blueprint(master.bp)
